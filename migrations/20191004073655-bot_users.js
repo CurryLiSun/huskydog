06174955ef56449526a6384ec31a79de72a250a7
currySun <justdomyselfli@gmail.com>
2019-10-04 15:46:03 +0800

    new feature t1

---
+++ migrations/20191004073655-bot_users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   return Promise.all([
+      queryInterface.addColumn(
+        'bot_users',
+        'groupNum',
+        {
+          type: Sequelize.INTEGER
+        }
+      ),
+      queryInterface.addColumn(
+        'bot_users',
+        'userAuth',
+        {
+          type: Sequelize.INTEGER
+        }
+      ),
+      queryInterface.addColumn(
+        'bot_users',
+        'enable',
+        {
+          type: Sequelize.BOOLEAN
+        }
+      ),
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};

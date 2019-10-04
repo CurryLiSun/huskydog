989528ddea839b3eb66d3300adeebde12a4b72f2
currySun <justdomyselfli@gmail.com>
2019-10-04 15:14:43 +0800

    new feature t1

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define("BotUsers", {
-        userId: DataTypes.STRING
+        userId: DataTypes.STRING,
+        groupNum: DataTypes.INTEGER,
+        userAuth: DataTypes.INTEGER,
+        enable: DataTypes.BOOLEAN
     },{
         tableName: 'bot_users', // this will define the table's name
         timestamps: false           // this will deactivate the timestamp columns
